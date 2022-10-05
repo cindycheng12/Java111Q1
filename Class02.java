01cdbcee6abc187ae6f068f3c35f79fd16a91832
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2022-10-05 23:32:39 +0800

    Update Class02.java

--- Class02.java
+++ Class02.java
@@ -1,26 +1,36 @@
-package test;
+package Java111Q1;
 class Data{
 	private String name;
 	private Test score;
 	public class Test{
 		int english;
 		int math;
-		   public Test(int eng,int m)      // 定義建構元Test()
-		   {
-		      english=eng;
-		      math=m;
-		   }
-		    public double avg(){
-			   System.out.println("avg="+(english+math)/2);
-		   }
-
+		Test(int eng,int m)      // 定義建構元Test()
+		{
+		    english=eng;
+		    math=m;
+		}
+		double avg(){
+			return(english+math)/2;
+		}
 			}
     public void show() {
     	System.out.println("name="+name);
-    	System.out.println("English="+english);
+    	System.out.println("English="+score.english);
+        System.out.println("math="+score.math);
+        System.out.println("avg="+score.avg());
+    }
+    Data(String n,int eng,int m){
+        name=n;
+        score=new Test(eng,m);
     }
 
 }
 public class Class02 {
-	
+    public static void main(String[] args) {
+       Data a1 = new Data("Annie", 85, 92);
+       a1.show();
+       Data a2 = new Data("Brian", 77, 56);
+       a2.show();
+    }
 }
