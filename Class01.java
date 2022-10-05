76ad91a81c40c8f13d4d7fc49a1f228542b08749
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2022-10-05 11:54:35 +0800

    Add files via upload

---
+++ Class01.java
@@ -0,0 +1,17 @@
+package test;
+class Cal_area{
+	double pi=3.14;
+	int l;
+	int width;
+	int height;
+	int radius;
+	
+	void Cal_area() {
+		
+		System.out.println("立方體表面積="+l*width*height)
+	}
+}
+public class Class01 {
+	Cal_area c= new Cal_area();
+	
+}
