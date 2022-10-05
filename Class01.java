99e1f7e8424c0d424d1db03f3350a076dfb24b7e
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2022-10-05 23:13:02 +0800

    Update Class01.java

--- Class01.java
+++ Class01.java
@@ -1,17 +1,34 @@
 package test;
-class Cal_area{
-	double pi=3.14;
-	int l;
-	int width;
-	int height;
-	int radius;
-	
-	void Cal_area() {
-		
-		System.out.println("立方體表面積="+l*width*height)
-	}
-}
-public class Class01 {
-	Cal_area c= new Cal_area();
-	
-}
+class area_volume {
+    double length;
+    double width;
+    double heigth;    
+    area_volume(double length,double width,double heigth){
+    this.length=length;
+    this.width=width;
+    this.heigth=heigth;
+    }
+    double area(){  //矩形面積
+    double b;
+    b=length*width;
+    return b;
+    }
+    double volume(){  //立方體體積
+    double b;
+    b=length*width*heigth;
+    return b;
+    }
+    
+    }
+
+    public class Class01 {
+        public static void main(String[] args){
+        area_volume a=new area_volume(2,5,0);
+        area_volume b=new area_volume(2,5,5);    
+        double c=a.area();   //a的面積
+        double d=b.volume();  //b的體積
+        System.out.println("矩形a="+"\n"+"長："+a.length+" "+"寬："+a.width+"\n"+"面積為："+c+"\n");
+        System.out.println("立方體b="+"\n"+"長："+b.length+" "+"寬："+b.width+"高："+b.heigth+"\n"+"體積為："+d+"\n");
+        }
+   }
+    
